5244244a2fba064c597e5ab6aedc303021369499
subbareddy <subbareddy4327@gmail.com>
2024-02-14 13:27:48 +0530

    first section of veena theme

---
+++ assets/custom-splide-corousel.js
@@ -0,0 +1,20 @@
+class CustomSplideCorousel extends HTMLElement {
+    constructor() {
+      super();
+  
+      this.element = this.querySelector(".splide");
+      this.splideList = this.querySelector(".splide__list");
+  
+      this.initializeSlider();
+    }
+  
+    initializeSlider() {
+      const options = JSON.parse(this.getAttribute("data-options"));
+  
+      var splide = new Splide(this.element, options);
+  
+      splide.mount();
+    }
+  }
+  
+  customElements.define("custom-splide-corousel", CustomSplideCorousel);  
